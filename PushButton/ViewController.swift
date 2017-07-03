12434f07aaf2c08f4c59af7ff1413871e0224283
gituxedo <sylvia.jin33@gmail.com>
2017-07-03 16:54:49 -0700

    Finished the greatest project ever
    Added fuzzy ducky functionality

--- PushButton/ViewController.swift
+++ PushButton/ViewController.swift
@@ -20,12 +20,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    var count:Int = 0
     @IBOutlet weak var pushMeButton: UIButton!
     @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var fuzzyDuckyLabel: UILabel!
     @IBAction func buttonPushed(_ sender: UIButton) {
-        var count:Int = 0
         count += 1
-        
+        countLabel.text = "Count: \(count)"
+        if count%15 == 0 {
+            fuzzyDuckyLabel.text = "FuzzyDucky"
+        } else if count%5 == 0 {
+            fuzzyDuckyLabel.text = "Ducky"
+        } else if count%3 == 0 {
+            fuzzyDuckyLabel.text = "Fuzzy"
+        } else {
+            fuzzyDuckyLabel.text = ""
+        }
     }
 }
 
